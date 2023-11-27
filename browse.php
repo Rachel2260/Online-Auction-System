927267bd857667faf067ffadea959368a128fe6a
linqingzhao <Rachel2260@outlook.com>
2023-11-27 22:46:42 +0000

    temporary

--- browse.php
+++ browse.php
@@ -75,20 +75,44 @@
 
 <ul class="list-group">
 <?php
+
+  if (!isset($_GET['order_by'])) {
+    $ordering = 'pricelow';
+  }
+  else {
+    $ordering = $_GET['order_by'];
+  }
+
+  $query = "SELECT * FROM Auction";
+  switch ($ordering) {
+    case 'pricelow': 
+      $query .= ' ORDER BY maxBidPrice ASC';
+      break;
+    case 'pricehigh':
+      $query .= ' ORDER BY maxBidPrice DESC';
+      break;
+    case 'date':
+      $$query .= ' ORDER BY A1.end_date ASC';
+      break;
+    // case 'datelate':
+    //   $$query .= ' ORDER BY A1.end_date DESC';
+    //   break;
+  }
   // Retrieve these from the URL
   if ((!isset($_GET['keyword']) && !isset($_GET['cat'])) | (isset($_GET['keyword']) && $_GET['keyword'] == '' && isset($_GET['cat']) && $_GET['cat'] == 'all')) {
-    $sql = "SELECT * FROM Auction";
-    $items = mysqli_query($conn,$sql);
-    $row_num = mysqli_num_rows($items);
-    if(mysqli_num_rows($items) == 0){
+    $query = "SELECT * FROM Auction";
+
+    $query_run = mysqli_query($conn,$query);
+
+    if(mysqli_num_rows($query_run) == 0){
       echo"There is no aution.";
       exit();
     }
-    elseif(!$items){
+    elseif(!$query_run){
       die("Error in auction query: " . mysqli_error($conn));
     }
 
-    while($row = mysqli_fetch_assoc($items)) : 
+    while($row = mysqli_fetch_assoc($query_run)) : 
       $item_id = $row['auction_ID'];
       $title = $row['item_name'];  
       $description = $row['description'];  
@@ -97,11 +121,13 @@
       $end_date = $row['end_time'];
       print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
     endwhile;
+    $num_results = mysqli_num_rows($items);
     // TODO: Define behavior if a keyword has not been specified.
   }
   elseif (isset($_GET['keyword']) && $_GET['keyword'] == '' && isset($_GET['cat']) && $_GET['cat'] != 'all'){
     $category = $_GET['cat'];
-    $query = "SELECT * FROM auction WHERE category = '$category'";
+    // $query = "SELECT * FROM auction WHERE category = '$category'";
+    $query .= "AND (category = '$category')";
     $query_run = mysqli_query($conn,$query);
     if (mysqli_num_rows($query_run)>0)
     {
@@ -123,9 +149,11 @@
         </tr>
       <?php
     }
+    $num_results = mysqli_num_rows($query_run);
   }elseif(isset($_GET['keyword']) && $_GET['keyword'] != '' && isset($_GET['cat']) && $_GET['cat'] == 'all'){
     $keyword = $_GET['keyword'];
-    $query = "SELECT * FROM auction WHERE CONCAT(item_name,description) LIKE '%$keyword%' ";
+    // $query = "SELECT * FROM auction WHERE CONCAT(item_name,description) LIKE '%$keyword%' ";
+    $query .= "AND (CONCAT(item_name,description) LIKE '%$keyword%')";
     $query_run = mysqli_query($conn,$query);
     if (mysqli_num_rows($query_run)>0)
     {
@@ -147,6 +175,7 @@
         </tr>
       <?php
     }
+    $num_results = mysqli_num_rows($query_run);
   }
   elseif(isset($_GET['keyword']) && $_GET['keyword'] != '' && isset($_GET['cat']) && $_GET['cat'] != 'all'){
     $category = $_GET['cat'];
@@ -154,7 +183,8 @@
     $keyword = $_GET['keyword'];
     // $query = "SELECT * FROM auction WHERE CONCAT(item_name,description) LIKE '%$keyword%' ";
 
-    $query = "SELECT * FROM auction WHERE category = '$category' AND CONCAT(item_name, description) LIKE '%$keyword%'";
+    // $query = "SELECT * FROM auction WHERE category = '$category' AND CONCAT(item_name, description) LIKE '%$keyword%'";
+    $query .= "AND (category = '$category' AND CONCAT(item_name, description) LIKE '%$keyword%')";
     $query_run = mysqli_query($conn,$query);
     if (mysqli_num_rows($query_run)>0)
     {
@@ -176,21 +206,17 @@
         </tr>
       <?php
     }
+    $num_results = mysqli_num_rows($query_run);
   }
 
-  if (!isset($_GET['cat'])) {
-    // TODO: Define behavior if a category has not been specified.
-  }
-  else {
-    $category = $_GET['cat'];
-  }
+  // if (!isset($_GET['cat'])) {
+  //   // TODO: Define behavior if a category has not been specified.
+  // }
+  // else {
+  //   $category = $_GET['cat'];
+  // }
   
-  if (!isset($_GET['order_by'])) {
-    // TODO: Define behavior if an order_by value has not been specified.
-  }
-  else {
-    $ordering = $_GET['order_by'];
-  }
+  
   
   if (!isset($_GET['page'])) {
     $curr_page = 1;
@@ -205,10 +231,59 @@
   
   /* For the purposes of pagination, it would also be helpful to know the
     total number of results that satisfy the above query */
-  $num_results = 96; // TODO: Calculate me for real
-  $results_per_page = 10;
+  // $num_results = 96; // TODO: Calculate me for real
+  $results_per_page = 3;
   $max_page = ceil($num_results / $results_per_page);
 ?>
+<!-- <?php
+  $keyword = $_GET['keyword'] ?? "";
+  $cat = $_GET['cat'] ?? "all";
+  $ordering = $_GET['order_by'] ?? "pricelow";
+  $curr_page = $_GET['page'] ?? 1;
+
+  $find_auctions_query = "SELECT A1.auction_Id, A1.item_name, A1.description, SQ.maxBidPrice, SQ.numBids, A1.end_time FROM Auction as A1,
+    (SELECT B.auction_Id, MAX(bid_price) as maxBidPrice, COUNT(DISTINCT B.bid_ID) as numBids FROM Auction as A2, Bid as B GROUP BY B.auction_Id) as SQ
+    WHERE A1.auction_Id = SQ.auction_Id"; //没bid过的不显示
+
+  // Add keyword filter to SQL query
+  $find_auctions_query .= " AND (A1.item_name LIKE '%{$keyword}%' OR A1.description LIKE '%{$keyword}%')";
+  // Add category filter to SQL query
+  if ($cat != 'all') {
+    $find_auctions_query .= " AND (A1.category = '{$cat}')";
+  }
+  // Add ordering filter to SQL query
+  switch ($ordering) {
+    case 'pricelow': 
+      $find_auctions_query .= ' ORDER BY maxBidPrice ASC';
+      break;
+    case 'pricehigh':
+      $find_auctions_query .= ' ORDER BY maxBidPrice DESC';
+      break;
+    case 'datesoon':
+      $find_auctions_query .= ' ORDER BY A1.end_date ASC';
+      break;
+    case 'datelate':
+      $find_auctions_query .= ' ORDER BY A1.end_date DESC';
+      break;
+  }
+
+  // Add pagination filter to SQL query
+  $query_run = mysqli_query($conn,$find_auctions_query);
+  $num_results = mysqli_num_rows($query_run);
+  $results_per_page = 10;
+  $max_page = ceil($num_results / $results_per_page);
+  $page_start_index = ($curr_page-1) * $results_per_page;  
+  $find_auctions_query .= " LIMIT " . $page_start_index . ',' . $results_per_page;
+  
+  // Execute Final Query
+  // print_h3($find_auctions_query);
+  $found_auctions = $query_run;
+  while($row = mysqli_fetch_array($found_auctions)) {
+    print_listing_li($row['auction_Id'], $row['item_name'], $row['description'], $row['maxBidPrice'], $row['numBids'], $row['end_time']);
+  } 
+?> -->
+
+
 <!-- TODO: Use a while loop to print a list item for each auction listing
      retrieved from the query -->
 
