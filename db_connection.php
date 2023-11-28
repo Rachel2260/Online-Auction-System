232ca894eefbae88c2320593ff7a3a1210a2c5f8
Ruohong Wei <143953845+Melaniewit@users.noreply.github.com>
2023-11-28 15:29:35 +0000

    update login and logout (redirect to login page)

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,15 @@
 <?php
   // Database Connection
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $database = "online_auction_system_1";
+  $servername = "localhost"; 
+
+$username = "Group22user"; 
+
+$password = "Group22"; 
+
+$database = "Online_Auction_System"; 
 
   try {
-      $conn = mysqli_connect($servername, $username, $password, $database);
+    $conn = mysqli_connect($servername, $username, $password, $database);
       if (!$conn) {
           throw new Exception("Connection failed: " . mysqli_connect_error());
       }
