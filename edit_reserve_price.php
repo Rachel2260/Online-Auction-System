430c0f6dcdc64d0803b1d16edca2e09fb10c6c6b
Yan Zhang <zczqyyz@ucl.ac.uk>
2023-11-29 05:33:44 +0000

    edit_reserve_prcie_change

---
+++ edit_reserve_price.php
@@ -0,0 +1,47 @@
+<?php require("utilities.php")?>
+<?php include_once("header.php");?>
+<?php include "db_connection.php";?>
+<div class="container my-5">
+<?php
+
+// TODO: Extract $_POST variables, check they're OK, and attempt to make a bid.
+// Notify user of success/failure and redirect/give navigation options.
+
+    // include "listing.php";
+    $auction_id = $_GET['item_id'];
+    $edit_reserve_price = $_POST['reserve_price'];
+    $sql_sp = "SELECT starting_price, reserve_price FROM auction WHERE auction_ID = $auction_id";
+    $result = mysqli_query($conn,$sql_sp);
+    $row = mysqli_fetch_assoc($result);
+    $starting_price = $row['starting_price'];
+    $original_reserve_price = $row['reserve_price'];
+    $current_price =  current_price($auction_id);
+    $user_id = $_SESSION["user_ID"];
+    $num_bid = count_bid($auction_id);
+
+    if ($num_bid ==0 and $starting_price < $edit_reserve_price){
+        $sql = "UPDATE auction
+                SET reserve_price = '$edit_reserve_price'
+                WHERE auction_ID = '$auction_id'";
+        if ($conn->query($sql) != TRUE) {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        } else{
+            echo('<div class="text-center">You edit successfully <a href="listing.php?item_id=' . $auction_id . '">Go Back.</a ></div>');
+        }
+    }
+    elseif ($num_bid !=0 and $current_price < $original_reserve_price ){
+        $sql = "UPDATE auction
+                SET reserve_price = '$edit_reserve_price'
+                WHERE auction_ID = '$auction_id'";
+        if ($conn->query($sql) != TRUE) {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        } else{
+            echo('<div class="text-center">You edit successfully!<a href="listing.php?item_id=' . $auction_id . '">Go Back</a ></div>');
+        }
+        
+    } 
+    else{
+        echo('<div class="text-center">You edit unsuccessfully, please can only set the reserve price if it is lower than the original reserve price! <a href="listing.php?item_id=' . $auction_id . '">Try again.</a ></div>');
+    }
+
+?>
